e58a18b5e3691cdf59d60af19688195e4d00ee60
krestfield <online@krestfield.com>
2020-09-08 14:52:24 +0100

    Updated to demoapi

--- src/main/java/com/krestfield/ezsign/samples/EzSignSample.java
+++ src/main/java/com/krestfield/ezsign/samples/EzSignSample.java
@@ -14,7 +14,7 @@ import java.util.Base64;
  * This sample demonstrates how the EzSign or PKCloud services can be called from a java client
  * A demonstration server is available at this location:
  *
- *    Server: demo.krestfield.com Port: 80
+ *    Server: demoapi.krestfield.com Port: 80
  *
  * And this sample targets that instance.  The demo server is up between the hours of 06:00 - 19:00 UTC
  * WARNING: This is for test purposes only, the certificates configured on the test server are for test only
